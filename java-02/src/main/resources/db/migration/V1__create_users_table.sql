cb2ffb49dc98e024a9749684061759dc2fc83b5f
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2025-06-19 20:08:25 +0900

    Spring Data JPA 시작하기

---
+++ java-02/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+   name VARCHAR(50) NOT NULL, // 이미 users 테이블 안에 있으니 username보다 name이 깔끔
+   email VARCHAR(255) NOT NULL UNIQUE,
+   password_hash VARCHAR(255) NOT NULL,
+
+   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
