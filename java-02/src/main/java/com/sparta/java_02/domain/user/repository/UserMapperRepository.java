db16e9f1fe5859d2e0e867f83ff7259b77704418
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2025-07-18 21:32:57 +0900

    MyBatis 활용하기 - 동적 SQL

--- java-02/src/main/java/com/sparta/java_02/domain/user/repository/UserMapperRepository.java
+++ java-02/src/main/java/com/sparta/java_02/domain/user/repository/UserMapperRepository.java
@@ -2,14 +2,16 @@ package com.sparta.java_02.domain.user.repository;
 
 import com.sparta.java_02.domain.user.dto.SearchUserDto;
 import com.sparta.java_02.domain.user.dto.UserDto;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapperRepository {
 
   SearchUserDto getUserById(Long id);
 
-  void insertUser(UserDto user);
+  void insertUser(@Param("users") List<UserDto> users);
 
   void updateUser(UserDto user);
 
