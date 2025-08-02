f9e42b4c00af06e5546e6911a1924bd92cac7cc7
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2025-08-02 18:23:02 +0900

    트랜잭션 격리 수준 실습

--- java-02/src/main/java/com/sparta/java_02/domain/product/entity/Product.java
+++ java-02/src/main/java/com/sparta/java_02/domain/product/entity/Product.java
@@ -1,10 +1,14 @@
 package com.sparta.java_02.domain.product.entity;
 
+import com.sparta.java_02.domain.category.entity.Category;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -32,13 +36,13 @@ public class Product {
   Long id;
 
   //TODO: 실습을  위해 임시로 주석처리
-  //@ManyToOne(fetch = FetchType.LAZY)
-  //@JoinColumn(name = "category_id")
-  //private Category category;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id")
+  Category category;
 
   //TODO: 실습을 위한 임시 컬럼입니다. (실제론 이렇게 작업하면 안됩니다.)
-  @Column(nullable = false)
-  Long categoryId;
+//  @Column(nullable = false)
+//  Long categoryId;
 
   @Column(nullable = false)
   String name;
@@ -62,13 +66,13 @@ public class Product {
 
   @Builder
   public Product(
-      Long categoryId,
+      Category category,
       String name,
       String description,
       BigDecimal price,
       Integer stock
   ) {
-    this.categoryId = categoryId;
+    this.category = category;
     this.name = name;
     this.description = description;
     this.price = price;
@@ -82,5 +86,11 @@ public class Product {
   public void increaseStock(Integer quantity) {
     this.stock += quantity;
   }
-  
+
+  public void setStock(Integer stock) {
+    if (stock > 0) {
+      this.stock = stock;
+    }
+  }
+
 }
